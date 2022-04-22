9017e9eed40e4e9973f563652ada89b2b19d7730
liyilin <liyilin2967@gmail.com>
2022-04-22 19:18:21 -0400

    add min_max play

---
+++ minmax_gobang_AI.py
@@ -0,0 +1,329 @@
+from minmax_graphics import *
+from math import *
+import numpy as np
+
+GRID_WIDTH = 40
+
+COLUMN = 8
+ROW = 8
+
+list0 = []  # AI 0（player 0）
+list1 = []  # AI 1（player 1）
+list2 = []  # human (player 2)
+list3 = []  # all
+
+list_all = []  
+next_point = [0, 0] 
+
+ratio = 1 
+DEPTH = 3 
+
+
+
+shape_score = [(50, (0, 1, 1, 0, 0)),
+               (50, (0, 0, 1, 1, 0)),
+               (200, (1, 1, 0, 1, 0)),
+               (500, (0, 0, 1, 1, 1)),
+               (500, (1, 1, 1, 0, 0)),
+               (5000, (0, 1, 1, 1, 0)),
+               (5000, (0, 1, 0, 1, 1, 0)),
+               (5000, (0, 1, 1, 0, 1, 0)),
+               (5000, (1, 1, 1, 0, 1)),
+               (5000, (1, 1, 0, 1, 1)),
+               (5000, (1, 0, 1, 1, 1)),
+               (5000, (1, 1, 1, 1, 0)),
+               (5000, (0, 1, 1, 1, 1)),
+               (50000, (0, 1, 1, 1, 1, 0)),
+               (99999999, (1, 1, 1, 1, 1))]
+
+
+def ai(is_ai):
+    global cut_count  
+    cut_count = 0
+    global search_count   
+    search_count = 0
+    negamax(is_ai, DEPTH, -99999999, 99999999)
+    #print("本次共剪枝次数：" + str(cut_count))
+    #print("本次共搜索次数：" + str(search_count))
+    return next_point[0], next_point[1]
+
+
+
+def negamax(is_ai, depth, alpha, beta):
+
+    if game_win(list0) or game_win(list1) or game_win(list2) or depth == 0:
+        return evaluation(is_ai)
+
+    blank_list = list(set(list_all).difference(set(list3)))
+    order(blank_list)   
+
+    for next_step in blank_list:
+
+        global search_count
+        search_count += 1
+
+        if not has_neightnor(next_step):
+            continue
+
+        if is_ai == 0:
+            list0.append(next_step)
+        elif is_ai == 1:
+            list1.append(next_step)
+        else:
+            list2.append(next_step)
+        list3.append(next_step)
+
+        value = -negamax(2-is_ai, depth - 1, -beta, -alpha)
+        if is_ai == 0:
+            list0.remove(next_step)
+        elif is_ai == 1:
+            list1.remove(next_step)
+        else :
+            list2.remove(next_step)
+        list3.remove(next_step)
+
+        if value > alpha:
+
+            print(str(value) + "alpha:" + str(alpha) + "beta:" + str(beta))
+            print(list3)
+            if depth == DEPTH:
+                next_point[0] = next_step[0]
+                next_point[1] = next_step[1]
+
+            if value >= beta:
+                global cut_count
+                cut_count += 1
+                return beta
+            alpha = value
+
+    return alpha
+
+
+
+def order(blank_list):
+    last_pt = list3[-1]
+    for item in blank_list:
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    continue
+                if (last_pt[0] + i, last_pt[1] + j) in blank_list:
+                    blank_list.remove((last_pt[0] + i, last_pt[1] + j))
+                    blank_list.insert(0, (last_pt[0] + i, last_pt[1] + j))
+
+
+def has_neightnor(pt):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            if (pt[0] + i, pt[1]+j) in list3:
+                return True
+    return False
+
+
+
+def evaluation(is_ai):
+    total_score = 0
+
+    if is_ai == 0:
+        my_list = list0
+        enemy_list = list1 + list2
+    elif is_ai == 1:
+        my_list = list1
+        enemy_list = list0 + list2
+    else:
+        my_list = list2
+        enemy_list = list0 + list1
+
+
+    score_all_arr = []  
+    my_score = 0
+    for pt in my_list:
+        m = pt[0]
+        n = pt[1]
+        my_score += cal_score(m, n, 0, 1, enemy_list, my_list, score_all_arr)
+        my_score += cal_score(m, n, 1, 0, enemy_list, my_list, score_all_arr)
+        my_score += cal_score(m, n, 1, 1, enemy_list, my_list, score_all_arr)
+        my_score += cal_score(m, n, -1, 1, enemy_list, my_list, score_all_arr)
+
+
+    score_all_arr_enemy = []
+    enemy_score = 0
+    for pt in enemy_list:
+        m = pt[0]
+        n = pt[1]
+        enemy_score += cal_score(m, n, 0, 1, my_list, enemy_list, score_all_arr_enemy)
+        enemy_score += cal_score(m, n, 1, 0, my_list, enemy_list, score_all_arr_enemy)
+        enemy_score += cal_score(m, n, 1, 1, my_list, enemy_list, score_all_arr_enemy)
+        enemy_score += cal_score(m, n, -1, 1, my_list, enemy_list, score_all_arr_enemy)
+
+    total_score = my_score - enemy_score*ratio*0.1
+
+    return total_score
+
+
+
+def cal_score(m, n, x_decrict, y_derice, enemy_list, my_list, score_all_arr):
+    add_score = 0  
+
+    max_score_shape = (0, None)
+
+
+    for item in score_all_arr:
+        for pt in item[1]:
+            if m == pt[0] and n == pt[1] and x_decrict == item[2][0] and y_derice == item[2][1]:
+                return 0
+
+    for offset in range(-5, 1):
+        # offset = -2
+        pos = []
+        for i in range(0, 6):
+            if (m + (i + offset) * x_decrict, n + (i + offset) * y_derice) in enemy_list:
+                pos.append(2)
+            elif (m + (i + offset) * x_decrict, n + (i + offset) * y_derice) in my_list:
+                pos.append(1)
+            else:
+                pos.append(0)
+        tmp_shap5 = (pos[0], pos[1], pos[2], pos[3], pos[4])
+        tmp_shap6 = (pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
+
+        for (score, shape) in shape_score:
+            if tmp_shap5 == shape or tmp_shap6 == shape:
+                if tmp_shap5 == (1,1,1,1,1):
+                    print('win')
+                if score > max_score_shape[0]:
+                    max_score_shape = (score, ((m + (0+offset) * x_decrict, n + (0+offset) * y_derice),
+                                               (m + (1+offset) * x_decrict, n + (1+offset) * y_derice),
+                                               (m + (2+offset) * x_decrict, n + (2+offset) * y_derice),
+                                               (m + (3+offset) * x_decrict, n + (3+offset) * y_derice),
+                                               (m + (4+offset) * x_decrict, n + (4+offset) * y_derice)), (x_decrict, y_derice))
+    if max_score_shape[1] is not None:
+        for item in score_all_arr:
+            for pt1 in item[1]:
+                for pt2 in max_score_shape[1]:
+                    if pt1 == pt2 and max_score_shape[0] > 10 and item[0] > 10:
+                        add_score += item[0] + max_score_shape[0]
+
+        score_all_arr.append(max_score_shape)
+
+    return add_score + max_score_shape[0]
+
+
+def game_win(list):
+    for m in range(COLUMN):
+        for n in range(ROW):
+
+            if n < ROW - 4 and (m, n) in list and (m, n + 1) in list and (m, n + 2) in list and (
+                    m, n + 3) in list and (m, n + 4) in list:
+                return True
+            elif m < ROW - 4 and (m, n) in list and (m + 1, n) in list and (m + 2, n) in list and (
+                        m + 3, n) in list and (m + 4, n) in list:
+                return True
+            elif m < ROW - 4 and n < ROW - 4 and (m, n) in list and (m + 1, n + 1) in list and (
+                        m + 2, n + 2) in list and (m + 3, n + 3) in list and (m + 4, n + 4) in list:
+                return True
+            elif m < ROW - 4 and n > 3 and (m, n) in list and (m + 1, n - 1) in list and (
+                        m + 2, n - 2) in list and (m + 3, n - 3) in list and (m + 4, n - 4) in list:
+                return True
+    return False
+
+
+def gobangwin():
+    win = GraphWin("this is a gobang game", GRID_WIDTH * COLUMN, GRID_WIDTH * ROW)
+    win.setBackground("yellow")
+    i1 = 0
+
+    while i1 <= GRID_WIDTH * COLUMN:
+        l = Line(Point(i1, 0), Point(i1, GRID_WIDTH * COLUMN))
+        l.draw(win)
+        i1 = i1 + GRID_WIDTH
+    i2 = 0
+
+    while i2 <= GRID_WIDTH * ROW:
+        l = Line(Point(0, i2), Point(GRID_WIDTH * ROW, i2))
+        l.draw(win)
+        i2 = i2 + GRID_WIDTH
+    return win
+
+
+def go():
+    win = gobangwin()
+
+    for i in range(COLUMN+1):
+        for j in range(ROW+1):
+            list_all.append((i, j))
+
+    change = 0
+    g = 0
+    m = 0
+    n = 0
+
+    while g == 0:
+
+        if change % 3 == 1:
+            pos = ai(0)
+
+            if pos in list3:
+                message = Text(Point(200, 200), "unavailable location" + str(pos[0]) + "," + str(pos[1]))
+                message.draw(win)
+                g = 1
+
+            list0.append(pos)
+            list3.append(pos)
+
+            piece = Circle(Point(GRID_WIDTH * pos[0], GRID_WIDTH * pos[1]), 16)
+            piece.setFill('white')
+            piece.draw(win)
+
+            if game_win(list0):
+                message = Text(Point(100, 100), "white win.")
+                message.draw(win)
+                g = 1
+            change = change + 1
+
+        elif change % 3 == 2:
+            pos = ai(1)
+
+            if pos in list3:
+                message = Text(Point(200, 200), "unavailable location" + str(pos[0]) + "," + str(pos[1]))
+                message.draw(win)
+                g = 1
+
+            list1.append(pos)
+            list3.append(pos)
+
+            piece = Circle(Point(GRID_WIDTH * pos[0], GRID_WIDTH * pos[1]), 16)
+            piece.setFill('green')
+            piece.draw(win)
+
+            if game_win(list1):
+                message = Text(Point(100, 100), "green win.")
+                message.draw(win)
+                g = 1
+            change = change + 1
+        else:
+            p2 = win.getMouse()
+            if not ((round((p2.getX()) / GRID_WIDTH), round((p2.getY()) / GRID_WIDTH)) in list3):
+
+                a2 = round((p2.getX()) / GRID_WIDTH)
+                b2 = round((p2.getY()) / GRID_WIDTH)
+                list2.append((a2, b2))
+                list3.append((a2, b2))
+
+                piece = Circle(Point(GRID_WIDTH * a2, GRID_WIDTH * b2), 16)
+                piece.setFill('black')
+                piece.draw(win)
+                if game_win(list2):
+                    message = Text(Point(100, 100), "black win.")
+                    message.draw(win)
+                    g = 1
+
+                change = change + 1
+
+    message = Text(Point(100, 120), "Click anywhere to quit.")
+    message.draw(win)
+    win.getMouse()
+    win.close()
+
+
